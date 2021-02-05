ef1a4cb289cfb6a7f5d1919d7869dcc8b0866d01
annie-thach <anniethach499@gmail.com>
2021-02-05 16:34:49 -0500

    Maze generation debugging

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,39 @@
 package com.company;
+import java.util.Random;
 
 public class Main {
+    private static int[][] maze;
+
+    public static void generateMaze(int dim, float den) {
+        maze = new int[dim][dim];
+        Random random = new Random();
+
+        for(int row = 0; row < maze.length; row++) {
+            for(int col = 0; col < maze[row].length; col++) {
+                // Exclude start and goal spaces.
+                if(!((row == 0 && col == 0) || (row == dim - 1 && col == dim - 1)) 
+                    && random.nextFloat() <= den) { // nextFloat() generates float betw. 0.0 & 1.0.
+                        maze[row][col] = 2;
+                }
+                else {
+                    maze[row][col] = 0;
+                }
+            }
+        }
+    }
+
+    public static void printMaze() {
+        for(int i = 0; i < maze.length; i++) {
+            for(int j = 0; j < maze[i].length; j++) {
+                System.out.print(maze[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 
     public static void main(String[] args) {
-	// write your code here
+    // write your code here
+        generateMaze(10, (float)0.5);
+        printMaze();
     }
 }
