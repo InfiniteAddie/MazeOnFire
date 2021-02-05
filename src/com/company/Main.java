47ef199ad5e8b0bd8d54fc09afa5e8912ae8305a
annie-thach <anniethach499@gmail.com>
2021-02-05 17:27:03 -0500

    DFS and copy function.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,27 @@
 package com.company;
+
 import java.util.Random;
 
+/**
+ * Let...
+ * 0 be an empty space
+ * 1 be an explored space
+ * 2 be an obstacle
+ * 3 be fire
+ * 
+ * Agent starts at (0, 0) and the goal is (dim - 1, dim - 1).
+ * 
+ */
 public class Main {
     private static int[][] maze;
 
-    public static void generateMaze(int dim, float den) {
+    /**
+     * Generate dim x dim maze given (dim)ension and (den)sity.
+     * Density is 0.0 < p < 1.0; p is the probability the tile will be filled.
+     * @param dim - Dimension of maze.
+     * @param den - Obstacle density.
+     */
+    public static void generateMaze(int dim, double den) {
         maze = new int[dim][dim];
         Random random = new Random();
 
@@ -12,7 +29,7 @@ public class Main {
             for(int col = 0; col < maze[row].length; col++) {
                 // Exclude start and goal spaces.
                 if(!((row == 0 && col == 0) || (row == dim - 1 && col == dim - 1)) 
-                    && random.nextFloat() <= den) { // nextFloat() generates float betw. 0.0 & 1.0.
+                    && random.nextDouble() <= den) { // nextDouble() generates double betw. 0.0 & 1.0.
                         maze[row][col] = 2;
                 }
                 else {
@@ -22,18 +39,45 @@ public class Main {
         }
     }
 
+    /**
+     * DFS search for goal.
+     * @return True if goal is reachable; false otherwise.
+     */
+    public boolean DFSMaze() {
+        
+        return false;
+    }
+
+    /**
+     * Makes a copy of the maze.
+     * @return Copy of maze.
+     */
+    public static int[][] copyMaze() {
+        int[][] copy = new int[maze.length][maze.length];
+
+        for(int row = 0; row < copy.length; row++) {
+            for(int col = 0; col < copy[row].length; col++) {
+                copy[row][col] = maze[row][col];
+            }
+        }
+
+        return copy;
+    }
+
+    /**
+     * Prints maze for debugging.
+     */
     public static void printMaze() {
-        for(int i = 0; i < maze.length; i++) {
-            for(int j = 0; j < maze[i].length; j++) {
-                System.out.print(maze[i][j] + " ");
+        for(int row = 0; row < maze.length; row++) {
+            for(int col = 0; col < maze[row].length; col++) {
+                System.out.print(maze[row][col] + " ");
             }
             System.out.println();
         }
     }
 
     public static void main(String[] args) {
-    // write your code here
-        generateMaze(10, (float)0.5);
+        generateMaze(10, 0.5);
         printMaze();
     }
 }
